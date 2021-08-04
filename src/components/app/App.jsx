db6ce5a0beb7b77ce64a3d0b0ed20c6d464ac369
culi <culitif@tuta.io>
2021-08-04 12:58:54 -0700

    setup router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import ListPage from '../../containers/ListPage';
+import DetailPage from '../../containers/DetailPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <main className="App">
+    <Router>
+      <Switch>
+
+        <Route exact path="/" render={routerProps => (
+          <ListPage {...routerProps}/>
+        )}/>
+
+        <Route exact path="/:id" render={routerProps => (
+          <DetailPage {...routerProps}/>
+        )}/>
+
+        <Redirect to="/"/>
+
+      </Switch>
+    </Router>
+  </main>;
 }
