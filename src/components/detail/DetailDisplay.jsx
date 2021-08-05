5b4377bd368a770795fa6e5db92d40276a814f9e
culi <culitif@tuta.io>
2021-08-04 18:06:09 -0700

    show all the data

--- src/components/detail/DetailDisplay.jsx
+++ src/components/detail/DetailDisplay.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './DetailDisplay.css';
+import { Link } from 'react-router-dom';
 
 const DetailDisplay = props => {
-  return <div className="DetailDisplay">
-    <span>{props.character.name}</span>
-    <span>|| {props.character.affiliation}</span>
-    <img src={props.character.photoUrl} />
-    <p>{props.character.allies.join(', ')}</p>
-    <p>{props.character.enemies.join(', ')}</p>
+  return <div className={styles.DetailDisplay}>
+    <span>name: <span className="info">{props.character.name}</span></span>
+    <img src={props.character.photoUrl}/>
+    <p>allies: 
+      <span className="info">{props.character.allies.join(', ')}</span>
+    </p>
+    <p>enemies: 
+      <span className="info">{props.character.enemies.join(', ')}</span>
+    </p>
+    {Object.keys(props.character)
+      .filter(k => !'_id name photoUrl allies enemies'.split(' ').includes(k))
+      .map(key => <>
+        <span>{key}: <span className="info">{props.character[key]}</span></span>
+      </>)
+    }
+    <Link to="/"><button>Back</button></Link>
   </div>;
 };
 
