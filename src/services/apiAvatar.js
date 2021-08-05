4b859384a0440c136f580540c2c18cc740794c34
culi <culitif@tuta.io>
2021-08-04 18:05:18 -0700

    fix getByCharId

--- src/services/apiAvatar.js
+++ src/services/apiAvatar.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-console */
-const API = 'https://last-airbender-api.herokuapp.com/api/v1/characters'
-  + '?perPage=69' 
-  + '&page='
-;
+const API = 'https://last-airbender-api.herokuapp.com/api/v1/characters';
 
-const getChars = async page => await fetch(API + page)
+
+const getChars = async page => await fetch(API + `?perPage=69&page=${page}`)
   .then(res => res.json())
   .catch(err => console.log(err))
 ;
 
-const getCharById = async (id) => await fetch(`${API}/${id}`)
+const getCharById = async id => await fetch(`${API}/${id}`)
   .then(res => res.json())
   .catch(err => console.log(err))
 ;
