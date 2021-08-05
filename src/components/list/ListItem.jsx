639920a231ef197da5c4dbb450c6697dbfe5d9b3
culi <culitif@tuta.io>
2021-08-04 18:06:03 -0700

    Link from list to detail

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const ListItem = props => {
   return <li className="ListItem">
-    <img src={props.character.photoUrl} alt={props.character.name}/>
-    {props.character.name}
+    <Link to={`/${props.character._id}`}>
+      <img src={props.character.photoUrl} alt={props.character.name}/>
+      {props.character.name}
+    </Link>
   </li>;
 };
 
